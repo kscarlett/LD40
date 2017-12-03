325de272400d372c814265d69f30d663de442613
kscarlett <kellen@kscarlett.com>
2017-12-03 01:08:34 +0000

    Fixed navmesh movement orientation

--- Assets/Scripts/AIBase.cs
+++ Assets/Scripts/AIBase.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent), typeof(Collider))]
-public class AIBase : MonoBehaviour {
+public class AIBase : MonoBehaviour
+{
+
+    [SerializeField] private bool _animate;
 
     //TODO: inject
     private Transform _targetTransform;
 
+    private NavMeshAgent _nav;
+    private Animator _anim;
+
 	// Use this for initialization
 	void Start () {
-		
+	    if (_animate)
+            _anim = GetComponentInChildren<Animator>();
+
+	    _nav = GetComponent<NavMeshAgent>();
+	    _targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (!(_animate && _anim.GetBool("Attacking")))
+        {
+            _nav.SetDestination(_targetTransform.position);
+        }
 	}
 }
