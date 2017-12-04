a80ae8029b751bd66c16820b3fcb5461efe6e851
kscarlett <kellen@kscarlett.com>
2017-12-04 21:13:24 +0000

    Added some audio effects

---
+++ Assets/Scripts/Testing/DebugAIMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DebugAIMover : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _nav;
+
+    void Start()
+    {
+        _nav.SetDestination(new Vector3(60, 0, 60));
+    }
+}
