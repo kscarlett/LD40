45f0630c51ba3ef68feac9e1b416e1606691cd6e
Matt Jones <matthewj866@gmail.com>
2017-12-04 01:39:27 +0000

    fix 2

--- Assets/Scripts/Installers/CastleInstaller.cs
+++ Assets/Scripts/Installers/CastleInstaller.cs
@@ -6,6 +6,6 @@ public class CastleInstaller : MonoInstaller<CastleInstaller>
     public override void InstallBindings()
     {
         Container.Bind<CastleBehaviour>().FromComponentInHierarchy();
-        Container.Bind<Transform>().FromComponentSibling();
+        Container.Bind<Transform>().FromResolveGetter<CastleBehaviour>(x => x.transform);
     }
 }
